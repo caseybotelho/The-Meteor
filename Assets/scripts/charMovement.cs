e220f4c073e809b12b2c86b845ccb1faed26d746
Casey <cb@shotgunflat.com>
2019-04-03 00:22:36 -0400

    Imported and cleaned some scripts, planning

---
+++ Assets/scripts/charMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charMovement : MonoBehaviour {
+
+    // main menu canvas
+    [SerializeField] private Canvas mainMenu;
+
+    // gamecontroller gameobject
+    [SerializeField] private GameObject controller;
+
+    // set rigidbody
+    private Rigidbody2D body;
+
+    // base movement speed
+    private const float baseSpeed = 6.0f;
+
+
+    void Start() {
+
+        // set rigidbody
+        body = GetComponent<Rigidbody2D>();
+
+        // cursor lock
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+
+    }
+
+    void Update() {
+        // set movement speed on input
+        float movY = Input.GetAxis("Vertical") * baseSpeed;
+        float movX = Input.GetAxis("Horizontal") * baseSpeed;
+
+        //movement
+        Vector2 velocity = new Vector2(movX, movY);
+        body.MovePosition(new Vector2(transform.position.x, transform.position.y) + velocity * Time.fixedDeltaTime);
+
+        // z position reset (might not be necessary for most setups, came from using 3d object in 2d space
+        if (transform.position.z != 0) {
+            Vector3 fixZ = transform.position;
+            fixZ.z = 0;
+            transform.position = fixZ;
+        }
+    }
+}
---
+++ Assets/scripts/maincamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maincamera : MonoBehaviour {
+
+	[SerializeField] private GameObject character;
+    [SerializeField] private GameObject house;
+
+    private bool hasMoved = false;
+    Vector3 housePos;
+
+    void Update () {
+        if (character.activeSelf == true) {
+		    transform.position = new Vector3(character.transform.position.x, character.transform.position.y, -1);
+        }
+        if (character.activeSelf == false && hasMoved == false) {
+            housePos = house.transform.position;
+            housePos.z = -999;
+            MoveCamera();
+        }
+	}
+
+    private void MoveCamera() {
+        this.transform.position = housePos;
+        hasMoved = true;
+    }
+}
